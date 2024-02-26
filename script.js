bba3298dfde09b329628ffad2f057a434c2742d7
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-26 14:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ const panels = document.querySelectorAll('.panel');
+
+    panels.forEach(panel => {
+      panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('expanding-card');
+      });
+    });
+
+    function removeActiveClasses() {
+      panels.forEach(panel => {
+        panel.classList.remove('expanding-card');
+      });
+    }
